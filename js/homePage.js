50f0509f69b73ea0558a1a44d109b5a3011cd085
shephali09 <hiremathshephali9@gmail.com>
2023-05-22 12:13:06 +0530

    Add: Ability to view Employee Payroll details in a Tabular Format from JSON Object. - Use Template Literals to populate the inner HTML.

--- js/homePage.js
+++ js/homePage.js
@@ -1,27 +1,74 @@
-//UC4
-window.addEventListener('DOMContentLoaded',(event) =>{
+//UC5
+window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
-});
-//Template literal ES6 feature
+  });
+
 const createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Action</th>";
-    const innerHtml = `${headerHtml}
-        
-            <tr>
-                <td><img class="profile" src="/Assets/profile-images/Ellipse -3.png" alt=""s></td>
-                <td>Ashwath</td>
-                <td>Male</td>
-                <td>
-                    <div class='dept-label'>HR</div>
-                    <div class="dept-label">Finance</div>
-                </td>
-                <td>3000000</td>
-                <td>1 Nov 2020</td>
-                <td>
-                    <img id="1" onclick="remove(this)" src="../Assets/icons/delete-black-18dp.svg" alt="delete">
-                    <img id="1" onclick="update(this)" src="../Assets/icons/create-black-18dp.svg" alt="edit">
-                </td>
-            </tr>
-    `;
-    document.querySelector('#table-display').innerHTML = innerHtml;
+
+const headerHtml =
+       `<th>Profile</th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Department</th>
+        <th>Salary</th>
+        <th>Start Date</th>
+        <th>Actions</th>`;
+
+let innerHtml = `${headerHtml}`;
+let empPayrollList = createEmployeePayrollJSON();
+
+for(const empPayrollData of empPayrollList){
+innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" src="${empPayrollData._profileImage}" alt="profile_img-1"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDepartmentHtml(empPayrollData._department)}</td>
+       
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+        <img name="${empPayrollData._id}" src="../Assets/icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+        <img name="${empPayrollData._id}" src="../Assets/icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+        </td>
+    </tr>`;
 }
+document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+      {
+        _name: 'Joe',
+        _gender: 'Female',
+        _department: [
+          'HR'
+        ],
+        _salary: 350000,
+        _startDate: '20 Jul 2016',
+        _note: 'Test',
+        _id: new Date().getTime(),
+        _profileImage: '../Assets/profile-images/Ellipse -4.png'
+      },
+      {
+        _name: 'Karthik',
+        _gender: 'Male',
+        _department: [
+          'Engineer'
+        ],
+        _salary: 400000,
+        _startDate: '02 Mar 2020',
+        _note: 'Test to JSON',
+        _id: new Date().getTime(),
+        _profileImage: '../Assets/profile-images/Ellipse -3.png'
+      }
+    ];
+    return empPayrollListLocal;
+  }
+  const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+      deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+  }
